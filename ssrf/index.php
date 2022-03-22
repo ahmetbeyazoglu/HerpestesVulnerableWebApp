234d58fd5df5debfbe6ffb8ef676bd1792a53dda
herpestes <46286059+herpestes0@users.noreply.github.com>
2022-03-22 22:18:40 +0300

    Update index.php

--- ssrf/index.php
+++ ssrf/index.php
@@ -1,4 +1,14 @@
 <?php
+// Initialize the session
+session_start();
+
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: ../login.php");
+    exit;
+}
+?>
+<?php
 
 if(isset($_GET ['url'])){
     $url= $_GET['url'];
