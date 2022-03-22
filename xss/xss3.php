476a9c54079fc5f855db4b18879483290788e95a
herpestes <46286059+herpestes0@users.noreply.github.com>
2022-03-22 22:18:10 +0300

    Update xss3.php

--- xss/xss3.php
+++ xss/xss3.php
@@ -1,6 +1,16 @@
+<?php
+// Initialize the session
+session_start();
+
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: ../login.php");
+    exit;
+}
+?>
 <!--Stored XSS-->
 
-<!--Default xss-->
+
 <!doctype html>
 <html lang="tr-TR">
     <head>
