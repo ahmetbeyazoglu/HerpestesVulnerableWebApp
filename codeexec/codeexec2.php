d2610d931c5e5eed72f51096e8c39acf3f8bd551
herpestes <46286059+herpestes0@users.noreply.github.com>
2022-03-22 22:22:21 +0300

    Update codeexec2.php

--- codeexec/codeexec2.php
+++ codeexec/codeexec2.php
@@ -1,4 +1,13 @@
-<!--Default xss-->
+<?php
+// Initialize the session
+session_start();
+
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: ../login.php");
+    exit;
+}
+?>
 <!doctype html>
 <html lang="tr-TR">
     <head>
