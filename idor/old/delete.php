ea198368f428a218937b83046414cfb0232bbc38
herpestes <46286059+herpestes0@users.noreply.github.com>
2022-03-22 22:19:44 +0300

    Delete idor/old directory

--- idor/old/delete.php
+++
@@ -1,4 +0,0 @@
-<?php
-session_start();
-//yapılan labları incele profil ve hesap silme için idor oluştur.
-?>
--- idor/old/olnew/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-    // Enter your host name, database username, password, and database name.
-    // If you have not set database password on localhost then set empty.
-    $con = mysqli_connect("localhost","herpestes","112263ABC","herpestes");
-    // Check connection
-    if (mysqli_connect_errno()){
-        echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    }
-?>
