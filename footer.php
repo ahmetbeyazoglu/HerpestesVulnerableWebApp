9f900a397d834f82073a15a8545c98a67b35f2f4
herpestes <46286059+herpestes0@users.noreply.github.com>
2022-05-12 01:08:28 +0300

    Delete footer.php

--- footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="bg-light text-center text-lg-start">
-  <!-- Copyright -->
-  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-  
-    <a class="text-dark" href="#">Herpestes Security</a>
-  </div>
-  <!-- Copyright -->
-</footer>
